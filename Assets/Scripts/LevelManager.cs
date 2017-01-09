e528e46717b092f5df4615dc3eb80a70f8b087ae
dionysus <dionysus.cho@gmail.com>
2017-01-08 22:45:51 -0800

    Initial Push - finished tutorial

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	//public to make accessible outside of class
+	public void LoadLevel (string name) {  
+		
+		Debug.Log ("LoadLevel requested for:" + name); //use debug.log to output to console for testing
+
+		SceneManager.LoadScene (name);
+		//Application.LoadLevel (name); //load level by name of scene (outdated)
+
+
+	}
+
+	public void QuitRequest () {  //dont need anything in brackets as we wont be requiring a name
+
+		Debug.Log ("QuitRequest requested");
+
+		Application.Quit ();
+
+	}
+
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NumberWizard : MonoBehaviour {
+
+	int max;
+	int min;
+	int guess;
+
+	public int guessMax = 10;
+	int guessCount = 0;
+
+
+	public Text text;
+
+	void Start () {
+
+		StartGame ();
+
+	}
+
+	void StartGame () {
+
+		max = 1000;
+		min = 1;
+
+		NextGuess ();
+
+	}
+		
+	public void GuessHigher(){
+		
+		min = guess;
+		NextGuess ();
+		++ guessCount;
+
+	}
+
+	public void GuessLower(){
+		
+		max = guess;
+		NextGuess ();
+
+	}
+
+	void NextGuess () {
+		
+		//guess = (max + min) / 2;
+
+		guess = Random.Range (min, max);
+
+		//text.text = "Is it " + guess + "?";
+
+		text.text = guess.ToString ();
+
+		if (guessCount >= guessMax) {
+			SceneManager.LoadScene ("Win");
+		}
+	}
+}
